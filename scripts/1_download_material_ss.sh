fec5cbb47c5c3647a9379c4c6414d0868d014c92
nturpaul <nicolas.turpault@inria.fr>
2020-03-12 15:51:14 +0100

    Removing cd and fixing paths

--- scripts/1_download_material_ss.sh
+++ scripts/1_download_material_ss.sh
@@ -4,7 +4,11 @@
 
 # We assume you're in the right conda envrionment
 
+DATASET_DIR=../dataset
+
 ########### Shouldn't be changed after that (except if you want TUT background too)
+
+SCRIPTS_PATH=../data_generation
 
 #############
 # DESED recorded data
@@ -12,33 +16,21 @@
 
 # If not already done
 echo "download real data audio files ... ~23GB"
-cd data_generation
-python download_real.py --basedir=../dataset
-cd ..
-
-# Copy or move data
-cp -r metadata ${ROOTDIR}/
-cp -r missing_files ${ROOTDIR}/
-echo "moving real audio files"
-mkdir -p ${ROOTDIR}/audio/
-mv audio/* ${ROOTDIR}/audio/
+python ${SCRIPTS_PATH}/download_real.py --basedir=${DATASET_DIR}
 
 #############
 # DESED Soundbank
 #############
-mkdir synthetic
-cd synthetic
 echo "Download and extract soundbank"
 wget -O DESED_synth_soundbank.tar.gz https://zenodo.org/record/3702397/files/DESED_synth_soundbank.tar.gz?download=1
 tar -xzf DESED_synth_soundbank.tar.gz
 #rm DESED_synth_soundbank.tar.gz
+mv synthetic ../synthetic
 echo "Done"
 
-cd data_generation
 # If you did not download the synthetic training background yet
 echo "Download SINS background... (to add TUT, add the option --TUT)"
-python get_background_training.py --basedir=../synthetic
-cd ..
+python ${SCRIPTS_PATH}/get_background_training.py --basedir=../synthetic
 echo "Done"
 
 
